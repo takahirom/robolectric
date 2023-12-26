719e132388dc6c7a9c4128a63f65fd4d18023084
takahirom <takam.dev@gmail.com>
2023-12-26 11:22:44 +0900

    Use reference images for Roborazzi's integration test

--- integration_tests/roborazzi/src/test/java/org/robolectric/integration/roborazzi/RoborazziCaptureTest.kt
+++ integration_tests/roborazzi/src/test/java/org/robolectric/integration/roborazzi/RoborazziCaptureTest.kt
@@ -4,27 +4,21 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.Application
 import android.content.ComponentName
-import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
 import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
-import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
-import com.github.takahirom.roborazzi.DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
 import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.RoborazziOptions
-import com.github.takahirom.roborazzi.RoborazziTaskType
+import com.github.takahirom.roborazzi.RoborazziRule
 import com.github.takahirom.roborazzi.captureScreenRoboImage
-import com.github.takahirom.roborazzi.roboOutputName
-import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -44,90 +38,57 @@ import org.robolectric.annotation.GraphicsMode
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 @OptIn(ExperimentalRoborazziApi::class)
 class RoborazziCaptureTest {
+  @get:Rule
+  val roborazziRule = RoborazziRule(
+    options = RoborazziRule.Options(
+      outputDirectoryPath = "src/screenshots",
+    )
+  )
+
   @Test
+  // For reducing repository size, we use small size
+  @Config(qualifiers = "w50dp-h40dp")
   fun checkViewWithElevationRendering() {
     hardwareRendererEnvironment {
       registerActivityToPackageManager(RoborazziViewWithElevationTestActivity::class.java.name)
-      val activityScenario =
-        ActivityScenario.launch(RoborazziViewWithElevationTestActivity::class.java)
+      ActivityScenario.launch(RoborazziViewWithElevationTestActivity::class.java)
 
       captureScreenWithRoborazzi()
-
-      // View the screenshots at build/output/roborazzi/*.png
-      val bitmap =
-        BitmapFactory.decodeFile("${DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH}/${roboOutputName()}.png")
-      activityScenario.onActivity { activity ->
-        val locationOnScreen = IntArray(2)
-        activity.view?.getLocationOnScreen(locationOnScreen)
-        // Check the background color of the FrameLayout
-        bitmap.getPixel(locationOnScreen[0] + 1, locationOnScreen[1] + 1).let {
-          assertThat(it).isEqualTo(activity.expectedViewBackgroundColor)
-        }
-      }
     }
   }
 
   @Test
+  // For reducing repository size, we use small size
+  @Config(qualifiers = "w110dp-h120dp")
   fun checkDialogRendering() {
     hardwareRendererEnvironment {
       registerActivityToPackageManager(RoborazziDialogTestActivity::class.java.name)
-      val activityScenario = ActivityScenario.launch(RoborazziDialogTestActivity::class.java)
+      ActivityScenario.launch(RoborazziDialogTestActivity::class.java)
 
       captureScreenWithRoborazzi()
-
-      // View the screenshots at build/output/roborazzi/*.png
-      val bitmap =
-        BitmapFactory.decodeFile("${DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH}/${roboOutputName()}.png")
-      activityScenario.onActivity { activity ->
-        val dialogContentView = activity.dialogContentView
-        val (left, top) = dialogContentView.getLocationInScreen(activity)
-        bitmap.getPixel(left + 1, top + 1).let {
-          assertThat(it).isEqualTo(activity.expectedDialogContentBackgroundColor)
-        }
-      }
     }
   }
 
   private fun captureScreenWithRoborazzi() {
-    captureScreenRoboImage(
-      roborazziOptions =
-        RoborazziOptions(
-          // roborazziOptions.taskType can modify the behavior.
-          // The default is RoborazziTaskType.None, meaning no screenshot capture.
-          // Normally, setting roborazziOptions.taskType to RoborazziTaskType.Record is unnecessary,
-          // as the Roborazzi Plugin automatically configures it.
-          // However, for this test case, we want to bypass the Roborazzi Plugin,
-          // focusing solely on the screenshot capture.
-          // Hence, we set roborazziOptions.taskType to RoborazziTaskType.Record.
-          taskType = RoborazziTaskType.Record
+    try {
+      captureScreenRoboImage(
+        roborazziOptions = RoborazziOptions(
+          recordOptions = RoborazziOptions.RecordOptions(
+            resizeScale = 0.5,
+          )
         )
-    )
+      )
+    } catch (e: AssertionError) {
+      throw AssertionError(
+        "Image changes are detected by Roborazzi. Please run `./gradlew :integration_tests:roborazzi:testDebugUnitTest` " +
+          "and commit images in integration_tests/roborazzi/src/screenshots", e
+      )
+    }
   }
 
   companion object {
     const val USE_HARDWARE_RENDERER_NATIVE_ENV = "robolectric.screenshot.hwrdr.native"
   }
-}
-
-private fun View?.getLocationInScreen(activity: RoborazziDialogTestActivity): Pair<Int, Int> {
-  val viewLocationOnWindow = IntArray(2)
-  // We can't get the location of the dialogContentView so we use the location in the window
-  // and calculate the location of the dialogContentView.
-  // This will return wrong value activity.dialogContentView?.getLocationOnScreen(locationOnScreen)
-  this?.getLocationInWindow(viewLocationOnWindow)
-  val windowLocationOnScreen = Rect()
-  val layoutParams = (this?.rootView?.layoutParams as WindowManager.LayoutParams)
-  Gravity.apply(
-    layoutParams.gravity,
-    this.rootView?.width ?: 0,
-    this.rootView?.height ?: 0,
-    Rect(0, 0, activity.window?.decorView?.width ?: 0, activity.window?.decorView?.height ?: 0),
-    windowLocationOnScreen
-  )
-  // Check the background color of the FrameLayout
-  val left = windowLocationOnScreen.left + viewLocationOnWindow[0]
-  val top = windowLocationOnScreen.top + viewLocationOnWindow[1]
-  return Pair(left, top)
 }
 
 private fun registerActivityToPackageManager(activity: String) {
@@ -176,13 +137,13 @@ private class RoborazziViewWithElevationTestActivity : Activity() {
             .apply {
               background = ColorDrawable(expectedViewBackgroundColor)
               elevation = 10f
-              addView(TextView(this.context).apply { text = "Roborazzi" })
+              addView(TextView(this.context).apply { text = "Txt" })
             }
             .also { view = it },
           LinearLayout.LayoutParams(
-              LinearLayout.LayoutParams.MATCH_PARENT,
-              LinearLayout.LayoutParams.MATCH_PARENT
-            )
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+          )
             .apply { setMargins(10.toDp(), 10.toDp(), 10.toDp(), 10.toDp()) }
         )
       }
@@ -194,6 +155,7 @@ private class RoborazziDialogTestActivity : Activity() {
   var dialogContentView: View? = null
   val expectedDialogContentBackgroundColor: Int = Color.MAGENTA
   override fun onCreate(savedInstanceState: Bundle?) {
+    setTheme(android.R.style.Theme_DeviceDefault_Light_NoActionBar)
     super.onCreate(savedInstanceState)
     setContentView(
       LinearLayout(this).apply {
@@ -204,22 +166,15 @@ private class RoborazziDialogTestActivity : Activity() {
         addView(
           TextView(this.context).apply { text = "Under the dialog" },
           LinearLayout.LayoutParams(
-              LinearLayout.LayoutParams.WRAP_CONTENT,
-              LinearLayout.LayoutParams.WRAP_CONTENT
-            )
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+          )
             .apply { setMargins(10.toDp(), 10.toDp(), 10.toDp(), 10.toDp()) }
         )
       }
     )
     AlertDialog.Builder(this)
-      .setTitle("Title")
-      .setView(
-        TextView(this).apply {
-          dialogContentView = this
-          text = "Roborazzi"
-          setBackgroundColor(expectedDialogContentBackgroundColor)
-        }
-      )
+      .setTitle("Dlg")
       .setPositiveButton("OK") { _, _ -> }
       .show()
   }
